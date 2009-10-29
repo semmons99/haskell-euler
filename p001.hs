2da8897d34407362b8a2147f713d27042c7a01cf
Shane Emmons <shane.emmons@fmins.com>
2009-10-29 07:53:30 -0400

    updated problem 001 to use intermediate functions

--- p001.hs
+++ p001.hs
@@ -1,4 +1,6 @@
-multiple_of n x = n `mod` x == 0
-multiple_of_3_or_5 n = multiple_of n 3 || multiple_of n 5
+multiple_of n = \x -> x `mod` n == 0
+multiple_of_3 = multiple_of 3
+multiple_of_5 = multiple_of 5
+multiple_of_3_or_5 n = multiple_of_3 n || multiple_of_5 n
 
 p001 = foldl1 (+) $ filter multiple_of_3_or_5 [1..999]
